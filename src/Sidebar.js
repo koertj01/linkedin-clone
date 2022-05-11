f89a5d46fb1a79ab158ac3e2b29e130f3bf3014b
Josh Koert <koertj01@gmail.com>
2022-05-10 22:38:13 -0400

    Resolved error

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
-import { Avatar } from "@material-ui/core/Avatar";
+import { Avatar } from "@material-ui/core";
 
 function Sidebar() {
   const recentItem = (topic) => (
